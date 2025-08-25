14b3305be663e3da69f81dc81a85a34426419d87
Karandeep Singh <karanmalhii1313@gmail.com>
2025-08-25 16:12:50 +0530

    first commit

---
+++ change color/script.js
@@ -0,0 +1,10 @@
+const buttons = document.querySelectorAll(".button");
+const body = document.querySelector("body");
+
+buttons.forEach(function (button) {
+  button.addEventListener("click", function (e) {
+    const color = e.target.id;
+
+    body.style.background = color;
+  });
+});
